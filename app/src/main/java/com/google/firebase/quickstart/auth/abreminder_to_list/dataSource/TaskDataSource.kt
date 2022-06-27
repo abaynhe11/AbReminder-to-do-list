7deb964796685dbb9bec88161503915d0ba9e7fc
Abaynhe Abitew <abaynheabitew@yahoo.com>
2022-06-27 10:30:27 -0500

    It has date and time ui but nothing else. We can add room database plus Task Manager to make it perfect

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/abreminder_to_list/dataSource/TaskDataSource.kt
@@ -0,0 +1,24 @@
+package com.google.firebase.quickstart.auth.abreminder_to_list.dataSource
+import com.google.firebase.quickstart.auth.abreminder_to_list.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list.toList()
+
+    fun insertTask(task: Task) {
+        if (task.id == 0) {
+            list.add(task.copy(id = list.size + 1))}
+        else {
+            list.remove(task)
+            list.add(task)
+        }
+    }
+
+    fun findById(taskId: Int) = list.find { it.id == taskId }
+
+    fun deleteTask(task: Task) {
+        list.remove(task)
+    }
+
+}
